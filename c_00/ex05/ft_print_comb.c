edd7119e2cf75248b3494bf687b28b6a26c7f27b
kido5285 <75468540+kido5285@users.noreply.github.com>
2025-03-03 22:19:04 +0800

    Update ft_print_comb.c

--- c_00/ex05/ft_print_comb.c
+++ c_00/ex05/ft_print_comb.c
@@ -15,32 +15,38 @@
 
 void	ft_print_comb(void);
 
-void	ft_print_comb(void)
+void    ft_print_comb(void)
 {
-	char	dig[12];
-	int	i;
-	int	j;
-	int	k;
+        char    dig[12];
+        int     i;
+        int     j;
+        int     k;
 
-	strcpy(dig, "0123456789");
-	i = 0;
-	j = 0;
-	k = 0;
-	while (i <= 90)
-	{
-		while (j <= 20)
-		{
-			j++;
-			while(k <= 20){
-				write(1, &dig[i], 1);
-				write(1, &dig[j], 1);
-				write(1, &dig[k], 1);
-				write(1, ", ", 2);
-				k++;
-			}
-		}
-		i++;
-	}
+        strcpy(dig, "0123456789");
+        i = 0;
+        while   (i <= 7)
+        {
+                j = 1;
+                while   (j <= 8)
+                {
+                        k = 2;
+                        while   (k <= 9)
+                        {
+                                if (j == k || i == k)
+                                        k++;
+                                else if (j == i)
+                                        i++;
+
+                                write(1, &dig[i], 1);
+                                write(1, &dig[j], 1);
+                                write(1, &dig[k], 1);
+                                write(1, " ,", 3);
+                                k++;
+                        }
+                        j++;
+                }
+                i++;
+        }
 }
 
 int	main(void)
